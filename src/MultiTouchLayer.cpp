f448901c58fbf97744a453687f2bac3adfec6417
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2025-08-20 14:07:41 -0400

    Old way

--- src/MultiTouchLayer.cpp
+++ src/MultiTouchLayer.cpp
@@ -34,80 +34,90 @@ bool MultiTouchLayer::isSwiping() {
     return (m_editorUI->m_swipeEnabled || CCKeyboardDispatcher::get()->getShiftKeyPressed()) && m_editorUI->m_selectedMode == 3;
 }
 
-bool MultiTouchLayer::ccTouchBegan(CCTouch* touch, CCEvent* event) {
-    bool isFirst = false;
-
+bool MultiTouchLayer::ccTouchBegan(CCTouch *pTouch, CCEvent *pEvent) {
+    m_touchCount++;
     if (!m_firstTouch) {
-        m_firstTouch = touch;
-        isFirst = true;
-    }
-    else if (!m_secondTouch) {
-        m_secondTouch = touch;
+        m_firstTouch = pTouch;
     }
 
-    if (isFirst || m_editorUI->m_editorLayer->m_playbackMode == PlaybackMode::Playing || isSwiping()) {
-        m_editorUI->ccTouchBegan(touch, event);
+    if (m_touchCount <= 1 || m_editorUI->m_editorLayer->m_playbackMode == PlaybackMode::Playing || isSwiping()) {
+        m_editorUI->ccTouchBegan(pTouch, pEvent);
         return true;
     }
+    else {
+        m_editorUI->m_isDraggingCamera = false;
+        m_editorUI->stopActionByTag(123);
+    }
 
-    m_editorUI->m_isDraggingCamera = false;
-    m_editorUI->stopActionByTag(123);
-
-    if (auto* editorUI = static_cast<MyEditorUI*>(EditorUI::get()); m_firstTouch != touch) {
-        auto fields = editorUI->m_fields.self();
-        fields->m_rotateDragging = true;
-        fields->m_lastPos = touch->getLocation();
+    if (MyEditorUI* editorUI = static_cast<MyEditorUI*>(EditorUI::get())) {
+        if (m_firstTouch != pTouch) {
+            auto fields = editorUI->m_fields.self();
+            fields->m_rotateDragging = true;
+            fields->m_lastPos = pTouch->getLocation();
+        }
     }
 
     return true;
 }
 
-void MultiTouchLayer::ccTouchMoved(CCTouch* touch, CCEvent* event) {
-    if ((!m_secondTouch && m_firstTouch) || m_editorUI->m_editorLayer->m_playbackMode == PlaybackMode::Playing || isSwiping()) {
-        m_editorUI->ccTouchMoved(touch, event);
-        return;
-    }
-    if (touch == m_firstTouch) {
+void MultiTouchLayer::ccTouchMoved(CCTouch *pTouch, CCEvent *pEvent) {
+    if (m_touchCount <= 1 || m_touchCount > 2 || m_editorUI->m_editorLayer->m_playbackMode == PlaybackMode::Playing || isSwiping()) {
+        m_editorUI->ccTouchMoved(pTouch, pEvent);
         return;
     }
 
-    if (auto* editorUI = static_cast<MyEditorUI*>(EditorUI::get())) {
+    if (pTouch == m_firstTouch) {
+        return;
+    }
+
+    if (MyEditorUI* editorUI = static_cast<MyEditorUI*>(EditorUI::get())) {
         auto fields = editorUI->m_fields.self();
-        if (!fields->m_rotateDragging) return;
+        if (fields->m_rotateDragging) {
 
-        const CCPoint currentPos = touch->getLocation();
-        const CCPoint center = (m_firstTouch->getLocation() + currentPos) / 2;
+            auto currentPos = pTouch->getLocation();
 
-        auto angle = [](const CCPoint& p) {
-            return atan2f(p.y, p.x);
-        };
+            CCPoint center = (m_firstTouch->getLocation() - -currentPos) / 2;
 
-        const auto v1 = fields->m_lastPos - center;
-        const auto v2 = currentPos - center;
+            auto v1 = fields->m_lastPos - center;
+            auto v2 = currentPos - center;
 
-        float deltaAngle = CC_RADIANS_TO_DEGREES(angle(v2) - angle(v1));
-        if (deltaAngle > 180.f) deltaAngle -= 360.f;
-        if (deltaAngle < -180.f) deltaAngle += 360.f;
+            float angle1 = atan2f(v1.y, v1.x);
+            float angle2 = atan2f(v2.y, v2.x);
+            float deltaAngle = CC_RADIANS_TO_DEGREES(angle2 - angle1);
 
-        editorUI->updateCanvasRotation(deltaAngle);
-        fields->m_lastPos = currentPos;
+            if (deltaAngle > 180.f) deltaAngle -= 360.f;
+            if (deltaAngle < -180.f) deltaAngle += 360.f;
+
+            editorUI->updateCanvasRotation(deltaAngle);
+
+            fields->m_lastPos = currentPos;
+        }
     }
 }
 
-void MultiTouchLayer::ccTouchEnded(CCTouch* touch, CCEvent* event) {
-    if (touch == m_firstTouch) m_firstTouch = nullptr;
-    else if (touch == m_secondTouch) m_secondTouch = nullptr;
+void MultiTouchLayer::ccTouchEnded(CCTouch *pTouch, CCEvent *pEvent) {
+    m_touchCount--;
 
-    if (!m_secondTouch || m_editorUI->m_editorLayer->m_playbackMode == PlaybackMode::Playing || isSwiping()) {
-        m_editorUI->ccTouchEnded(touch, event);
+    if (pTouch == m_firstTouch) {
+        m_firstTouch = nullptr;
+    }
+
+    if (m_touchCount < 1 || m_editorUI->m_editorLayer->m_playbackMode == PlaybackMode::Playing || isSwiping()) {
+        m_editorUI->ccTouchEnded(pTouch, pEvent);
+        return;
     }
 }
 
-void MultiTouchLayer::ccTouchCancelled(CCTouch* touch, CCEvent* event) {
-    if (touch == m_firstTouch) m_firstTouch = nullptr;
-    else if (touch == m_secondTouch) m_secondTouch = nullptr;
+void MultiTouchLayer::ccTouchCancelled(CCTouch *pTouch, CCEvent *pEvent) {
+    m_touchCount--;
 
-    if (!m_secondTouch || m_editorUI->m_editorLayer->m_playbackMode == PlaybackMode::Playing || isSwiping()) {
-        m_editorUI->ccTouchCancelled(touch, event);
+    if (pTouch == m_firstTouch) {
+        m_firstTouch = nullptr;
     }
+
+    if (m_touchCount < 1 || m_editorUI->m_editorLayer->m_playbackMode == PlaybackMode::Playing || isSwiping()) {
+        m_editorUI->ccTouchCancelled(pTouch, pEvent);
+        return;
+    }
+
 }
