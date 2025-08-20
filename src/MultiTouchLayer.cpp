668a08292bcc6ca296a2fc1bfa9dca44c12a37c7
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2025-08-19 21:47:10 -0400

    Update MultiTouchLayer.cpp

--- src/MultiTouchLayer.cpp
+++ src/MultiTouchLayer.cpp
@@ -41,7 +41,8 @@ bool MultiTouchLayer::ccTouchBegan(CCTouch *pTouch, CCEvent *pEvent) {
         return true;
     }
     else {
-        m_editorUI->ccTouchEnded(m_firstTouch, pEvent);
+        m_editorUI->m_isDraggingCamera = true;
+        m_editorUI->m_continueSwipe = false;
     }
 
     if (MyEditorUI* editorUI = static_cast<MyEditorUI*>(EditorUI::get())) {
